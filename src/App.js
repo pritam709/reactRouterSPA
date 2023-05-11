63b8f1de32fdf6079dd8973f96b498f133e5e276
Pravesh <pritamyadav70923@gmail.com>
2023-05-11 10:58:04 +0530

    2 routes created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-const router= createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+import ProductusPage from "./pages/products";
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductusPage /> },
+]);
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/products.js
@@ -0,0 +1,5 @@
+const ProductusPage =()=>{
+return <h1>welcome to products page</h1>
+}
+
+export default ProductusPage
