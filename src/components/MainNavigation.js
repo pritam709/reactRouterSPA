eea6ff0b618302ea0186247bc0d551c02d4025cf
Pravesh <pritamyadav70923@gmail.com>
2023-05-12 09:54:59 +0530

    navLinks instead of link

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import classes from "./MainNavigation.module.css"
 const MainNavigation = ()=>{
   return <header className={classes.header}>
     <nav>
         <ul className={classes.list}>
             <li>
-               <Link to="/"> Home </Link>
+               <NavLink to="/" className={({isActive})=>isActive ?classes.active :undefined}
+               end > Home </NavLink>
             </li>
             <li>
-               <Link to="/products"> Products </Link>
+               <NavLink to="/products"> Products </NavLink>
             </li>
         </ul>
     </nav>
